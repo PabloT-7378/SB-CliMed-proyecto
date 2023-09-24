62dcc039db72402544bf87ba4112aefef93027d6
PabloT-7378 <nellyantonio7378@gmail.com>
2023-09-24 19:07:45 -0300

    segundo

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultaService.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultaService.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import java.util.List;
+import med.voll.api.domain.consulta.validaciones.ValidadorDeConsultas;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
@@ -16,16 +18,20 @@ public class AgendaDeConsultaService {
     private MedicoRepository medicoRepository;
     @Autowired
     private ConsultaRepository consultaRepository;
+    @Autowired
+    List<ValidadorDeConsultas> validadores;
 
     public void agendar(DatosAgendarConsulta datos){
 
-        if(pacienteRepository.findById(datos.idPaciente()).isPresent()){
+        if(!pacienteRepository.findById(datos.idPaciente()).isPresent()){
             throw new ValidacionDeIntegridad("este id para el paciente no fue encontrado");
         }
 
-        if(datos.idMedico()!=null && medicoRepository.existsById(datos.idMedico())){
+        if(datos.idMedico()!=null && !medicoRepository.existsById(datos.idMedico())){
             throw new ValidacionDeIntegridad("este id para el medico no fue encontrado");
         }
+        
+        validadores.forEach(v->v.validar(datos));
 
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
 
